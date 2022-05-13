ae7b2173ae67ee8c863767170252f0b96227c6bb
Quinten Coret <Qcoret@gmail.com>
2022-05-14 00:09:57 +0200

    Changing vibe, and adding subtle mouse-follow

--- src/lib/Hero.svelte
+++ src/lib/Hero.svelte
@@ -3,12 +3,14 @@
   import { SplitText } from 'gsap/dist/SplitText.js';
   import { onMount } from 'svelte';
 
+  import { spring } from 'svelte/motion';
+
   onMount(() => {
-    const split = new SplitText('.qcoret h1', { type: 'lines' });
+    const split = new SplitText('.qc h1', { type: 'lines' });
     const tl = gsap.timeline();
 
     tl.fromTo(
-      '.qcoret',
+      '.qc',
       {
         clipPath: 'circle(0% at 0 50%);',
       },
@@ -22,51 +24,63 @@
     tl.fromTo(
       split.lines,
       {
-        x: 100,
+        x: '10vw',
         rotateY: 20,
       },
       {
         x: 0,
         rotateY: 0,
         duration: 2,
-        stagger: .15,
-        // force3D: true,
+        stagger: 0.1,
         ease: 'power3.out',
       },
-      "-=1.5"
+      '-=1.5'
     );
   });
+
+  const coords = spring({ x: 0, y: 0 }, { stiffness: 0.03, damping: 0.2 });
+
+  const handleMouseMove = (e) => {
+    coords.set({
+      x: (e.clientX / window.innerWidth) * -10 + 5,
+      y: (e.clientY / window.innerHeight) * -10 + 5,
+    });
+  };
 </script>
 
-<section>
-  <div class='qcoret'>
-    <h1>Quinten Coret</h1>
+<section on:mousemove={handleMouseMove} on:mousedown={() => console.log($coords)}>
+  <div style:transform="translate({$coords.x}px, {$coords.y}px)" class="qc">
+    <h1>quinten coret</h1>
   </div>
 </section>
 
 <style>
   section {
-    position: relative;
+    background: radial-gradient(circle, rgba(20, 1, 20, 1) 0%, rgba(22, 10, 22, 1) 100%);
+    display: flex;
+    flex-direction: column;
+    justify-content: center;
+    align-items: center;
     height: 100vh;
     width: 100vw;
   }
 
-  .qcoret {
-    position: absolute;
-    top: 50%;
-    left: 50%;
-    transform: translate(-50%, -50%);
+  .qc {
     width: min-content;
+    user-select: none;
   }
 
   h1 {
+    font-family: 'Newsreader', serif;
     margin: 0;
-    font-weight: 500;
-    font-size: 17rem;
-    line-height: 0.75;
+    color: #ffffff;
+    font-weight: 100;
+    font-size: 19vw;
+    line-height: 0.6;
     text-align: right;
     transform-origin: right;
     perspective: 100vh;
+    transform-style: preserve-3d;
   }
 
   .v {
