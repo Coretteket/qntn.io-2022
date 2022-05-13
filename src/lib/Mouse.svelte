08b3016daacb1c4b751f8838984c3e6417e48886
Quinten Coret <Qcoret@gmail.com>
2022-05-14 00:11:02 +0200

    Creating custom cursor

---
+++ src/lib/Mouse.svelte
@@ -0,0 +1,36 @@
+<script>
+  import { spring } from 'svelte/motion';
+
+  const coords = spring({ x: 0, y: 0 }, { stiffness: 0.2 });
+
+  const handleMouseMove = (e) => {
+    coords.set({
+      x: e.clientX,
+      y: e.clientY,
+    });
+  };
+</script>
+
+<svelte:window on:mousemove={handleMouseMove} />
+
+<div style:left="{$coords.x}px" style:top="{$coords.y}px" />
+
+<style>
+  :global(body) {
+    cursor: none;
+  }
+
+  div {
+    color: white;
+    position: fixed;
+    transform: translate(-50%, -50%);
+    border-radius: 100%;
+    background: rgba(20, 1, 20, 0.6);
+    border: solid 2px white;
+    width: 20px;
+    aspect-ratio: 1;
+    mix-blend-mode: exclusion;
+    pointer-events: none;
+    z-index: 99;
+  }
+</style>
--- src/routes/__layout.svelte
+++ src/routes/__layout.svelte
@@ -1,13 +1,15 @@
 <script>
+  import Mouse from '$lib/Mouse.svelte';
   import '../global.css';
+  import { onMount } from 'svelte';
+
+  let mobile = false;
+  onMount(() => {
+    mobile = 'ontouchstart' in document.documentElement;
+  });
 </script>
 
-<main>
-  <slot />
-</main>
-
-<style>
-  * {
-    font-family: 'EB Garamond', serif;
-  }
-</style>
+<slot />
+{#if !mobile}
+  <Mouse />
+{/if}
