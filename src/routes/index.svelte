c53ca2925e2b9c61434a1d95b79fb10cb9ec8b1e
Quinten Coret <Qcoret@gmail.com>
2022-05-14 00:11:09 +0200

    Adding filler

--- src/routes/index.svelte
+++ src/routes/index.svelte
@@ -2,4 +2,19 @@
   import Hero from '$lib/Hero.svelte';
 </script>
 
-<Hero />
+<main>
+  <Hero />
+  {#each Array(10) as i}
+    <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Sed cumque nam ea officia possimus dolores quasi. Consequatur aperiam aliquid, illo veniam mollitia sunt modi blanditiis adipisci laboriosam quos. Distinctio, quos. Lorem ipsum dolor sit amet consectetur adipisicing elit. Sed cumque nam ea officia possimus dolores quasi. Consequatur aperiam aliquid, illo veniam mollitia sunt modi blanditiis adipisci laboriosam quos. Distinctio, quos. Lorem ipsum dolor sit amet consectetur adipisicing elit. Sed cumque nam ea officia possimus dolores quasi. Consequatur aperiam aliquid, illo veniam mollitia sunt modi blanditiis adipisci laboriosam quos. Distinctio, quos.</p>
+  {/each}
+</main>
+
+<style>
+  p {
+    font-family: 'Newsreader', serif;
+    font-weight: 300;
+    font-size: 22px;
+    margin: 2em auto;
+    width: min(800px, 90vw);
+  }
+</style>
