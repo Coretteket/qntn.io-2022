2e1431d9a0f3f786187f77ab9dc4bc82ed59cea9
Quinten Coret <Qcoret@gmail.com>
2022-05-20 00:05:37 +0200

    Bugfix?

--- src/hooks.js
+++ src/hooks.js
@@ -1,6 +1,6 @@
 export async function handle({ event, resolve }) {
-  const acceptLanguageHeader = event.request.headers.get('accept-language');
-  const locale = acceptLanguageHeader.substring(0, 2) === 'nl' ? 'nl' : 'en';
+  const acceptLanguageHeader = event.request.headers.get('accept-language') || '';
+  const locale = acceptLanguageHeader.substring(0, 2) == 'nl' ? 'nl' : 'en';
   event.locals = { locale };
   const rendered = await resolve(event, {
     transformPage: ({ html }) => html.replace('lang="en"', `lang="${locale}"`),
