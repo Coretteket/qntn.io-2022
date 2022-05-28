97da11c210518ee483639b3b39c3991998c9d541
Quinten Coret <Qcoret@gmail.com>
2022-05-28 21:11:26 +0200

    Bugfix?

--- svelte.config.js
+++ svelte.config.js
@@ -1,9 +1,9 @@
 import preprocess from 'svelte-preprocess';
-import adapter from '@sveltejs/adapter-auto';
+import adapter from '@sveltejs/adapter-vercel';
 
 const config = {
   kit: {
-    adapter: adapter(/*{ edge: true }*/),
+    adapter: adapter({ edge: false }),
   },
   preprocess: [
     preprocess({
