4f9cec9f2269274f6e46ef8f3c6dddd5d8da5571
Quinten Coret <Qcoret@gmail.com>
2022-05-31 19:25:30 +0200

    Switching back to adapter-auto

--- svelte.config.js
+++ svelte.config.js
@@ -1,11 +1,11 @@
-import adapter from '@sveltejs/adapter-vercel';
+import adapter from '@sveltejs/adapter-auto';
 import svg from '@poppanator/sveltekit-svg';
 import preprocess from 'svelte-preprocess';
 import path from 'path';
 
 const config = {
   kit: {
-    adapter: adapter({ edge: false }),
+    adapter: adapter(),
     vite: { plugins: [svg()], resolve: { alias: { $svg: path.resolve('./static/svg') } } },
   },
   preprocess: [preprocess({ postcss: true })],
