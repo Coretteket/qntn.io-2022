d20e368d24318a395e8ad59ea7d22445e21ea527
Quinten Coret <Qcoret@gmail.com>
2022-06-02 22:38:10 +0200

    Use experimental prefers-color-scheme header; smarter accept-language header parsing.

--- src/hooks.ts
+++ src/hooks.ts
@@ -1,23 +1,34 @@
-import { isType, locales, themes, type Locale, type Theme } from './scripts/types';
+import { isType, locales, themes, makeMutable, type Locale, type Theme } from './scripts/types';
 import type { Handle, GetSession } from '@sveltejs/kit';
+import { pick } from 'accept-language-parser';
 import cookie from 'cookie';
 
 export const handle: Handle = async ({ event, resolve }) => {
   const { headers } = event.request;
-  const acceptLang = headers.get('accept-language')?.substring(0, 2);
+
+  const acceptLang = headers.get('accept-language') || '';
+  const pickAcceptLang = pick(makeMutable(locales), acceptLang, { loose: true });
+  const acceptPrefers = headers.get('sec-ch-prefers-color-scheme');
   const cookies = cookie.parse(headers.get('cookie') || '');
 
-  const localeHeader = isType<Locale>(acceptLang, locales) ? acceptLang : 'en';
+  const localeHeader = pickAcceptLang || 'en';
   const locale = isType<Locale>(cookies.locale, locales) ? cookies.locale : localeHeader;
-  const theme = isType<Theme>(cookies.theme, themes) ? cookies.theme : 'auto';
+
+  const themeHeader = isType<Theme>(acceptPrefers, themes) ? acceptPrefers : 'auto';
+  const theme = isType<Theme>(cookies.theme, themes) ? cookies.theme : themeHeader;
 
   event.locals = { locale, theme };
 
-  const rendered = await resolve(event, {
+  const response = await resolve(event, {
     transformPage: ({ html }) =>
       html.replace('<html lang="en">', `<html lang="${locale}" data-theme="${theme}">`),
   });
-  return rendered;
+
+  // set experimental client hints
+  response.headers.set('Accept-CH', 'Sec-CH-Prefers-Color-Scheme');
+  response.headers.set('Vary', 'Sec-CH-Prefers-Color-Scheme');
+
+  return response;
 };
 
 export const getSession: GetSession = ({ locals }) => {
