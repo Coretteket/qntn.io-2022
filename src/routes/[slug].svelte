4301fa4a82b653d3328015069240a2deecd4167b
Quinten Coret <Qcoret@gmail.com>
2022-06-02 18:04:37 +0200

    Switching to URIComponent

--- src/routes/[slug].svelte
+++ src/routes/[slug].svelte
@@ -9,7 +9,7 @@
 
   export const load: Load = async ({ params }) => {
     const slug = params.slug as keyof typeof redir;
-    if (slug in redir) return { status: 302, redirect: encodeURI(redir[slug]) };
+    if (slug in redir) return { status: 302, redirect: encodeURIComponent(redir[slug]) };
     return { status: 404 };
   };
 </script>
