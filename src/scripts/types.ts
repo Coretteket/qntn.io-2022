e83518c14f3ec3712dc4245687e7ce33c5fe8ad0
Quinten Coret <Qcoret@gmail.com>
2022-06-03 13:48:38 +0200

    Optimisations

--- src/scripts/types.ts
+++ src/scripts/types.ts
@@ -1,8 +1,5 @@
 export type Translations = Record<Locale, Record<Route, Record<string, string>>>;
 
-export type Mutable<T> = { -readonly [P in keyof T]: T[P] };
-export const mutable = <T>(t: T): Mutable<T> => t as Mutable<T>;
-
 export const routes = ['global', 'index'] as const;
 export type Route = typeof routes[number];
 
@@ -12,4 +9,7 @@ export type Locale = typeof locales[number];
 export const themes = ['dark', 'light', 'auto'] as const;
 export type Theme = typeof themes[number];
 
-export const isType = <T>(t: any, l: readonly T[]): t is T => l.includes(t as T);
+export const isType = <T>(t: unknown, l: readonly T[]): t is T => l.includes(t as T);
+
+export type Mutable<T> = { -readonly [P in keyof T]: T[P] };
+export const mutable = <T>(t: T) => t as Mutable<T>;
