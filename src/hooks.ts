80d97eb24df92992988c5dd554f538f9ec1bc4a3
Quinten Coret <Qcoret@gmail.com>
2022-06-03 13:56:32 +0200

    Cleaner code

--- src/hooks.ts
+++ src/hooks.ts
@@ -1,32 +1,31 @@
-import { isType, locales, themes, mutable, type Locale, type Theme } from './scripts/types';
+import { isType, locales, themes, mutable } from './scripts/types';
 import type { Handle, GetSession } from '@sveltejs/kit';
 import { pick } from 'accept-language-parser';
-import cookie from 'cookie';
+import { parse } from 'cookie';
 
 export const handle: Handle = async ({ event, resolve }) => {
   const { headers } = event.request;
 
   const acceptLang = headers.get('accept-language') || '';
-  const pickAcceptLang = pick(mutable(locales), acceptLang, { loose: true });
   const acceptPrefers = headers.get('sec-ch-prefers-color-scheme');
-  const cookies = cookie.parse(headers.get('cookie') || '');
+  const cookies = parse(headers.get('cookie') || '');
 
-  const localeHeader = pickAcceptLang || 'en';
-  const locale = isType<Locale>(cookies.locale, locales) ? cookies.locale : localeHeader;
+  const localeHeader = pick(mutable(locales), acceptLang, { loose: true }) || 'en';
+  const locale = isType(cookies.locale, locales) ? cookies.locale : localeHeader;
 
-  const themeHeader = isType<Theme>(acceptPrefers, themes) ? acceptPrefers : 'auto';
-  const theme = isType<Theme>(cookies.theme, themes) ? cookies.theme : themeHeader;
+  const themeHeader = isType(acceptPrefers, themes) ? acceptPrefers : 'auto';
+  const theme = isType(cookies.theme, themes) ? cookies.theme : themeHeader;
 
   event.locals = { locale, theme };
 
   const response = await resolve(event, {
     transformPage: ({ html }) =>
-      html.replace('<html lang="en">', `<html lang="${locale}" data-theme="${theme}">`),
+      html.replace('<html>', `<html lang="${locale}" data-theme="${theme}">`),
   });
 
   // set experimental client hints
-  response.headers.set('Accept-CH', 'Sec-CH-Prefers-Color-Scheme');
-  response.headers.set('Vary', 'Sec-CH-Prefers-Color-Scheme');
+  response.headers.set('accept-ch', 'sec-ch-prefers-color-scheme');
+  response.headers.set('vary', 'sec-ch-prefers-color-scheme');
 
   return response;
 };
