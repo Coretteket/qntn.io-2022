f3da3ebaa627b12356ad04eaf6471ca9d8bdb066
Quinten Coret <Qcoret@gmail.com>
2022-06-04 17:45:43 +0200

    New and shiny alias syntax

--- svelte.config.js
+++ svelte.config.js
@@ -1,18 +1,13 @@
 import adapter from '@sveltejs/adapter-vercel';
 import svg from '@poppanator/sveltekit-svg';
 import preprocess from 'svelte-preprocess';
-import path from 'path';
 
 /** @type {import('@sveltejs/kit').Config} */
 const config = {
   kit: {
     adapter: adapter(),
-    vite: {
-      plugins: [svg()],
-      resolve: {
-        alias: { $svg: path.resolve('./static/svg') },
-      },
-    },
+    vite: { plugins: [svg()] },
+    alias: { $svg: 'static/svg' },
   },
   preprocess: [preprocess({ postcss: true })],
 };
