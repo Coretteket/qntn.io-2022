36bb81eb16ffb669c376b8df04bf5faf9afd3a94
Quinten Coret <Qcoret@gmail.com>
2022-06-20 17:37:06 +0200

    Moving slug handling to Vercel environment variables

--- src/routes/[slug].svelte
+++ src/routes/[slug].svelte
@@ -1,15 +1,9 @@
 <script context="module" lang="ts">
   import type { Load } from '@sveltejs/kit';
 
-  const redir = {
-    twitter: 'https://twitter.com/coretteket',
-    github: 'https://github.com/coretteket',
-    linkedin: 'https://www.linkedin.com/in/qcoret/',
-  };
-
-  export const load: Load = async ({ params }) => {
-    const slug = params.slug as keyof typeof redir;
-    if (slug in redir) return { status: 301, redirect: encodeURI(redir[slug]) };
+  export const load: Load = async ({ params: { slug } }) => {
+    const redir = import.meta.env[`VITE_SLUG_${slug.toUpperCase()}`];
+    if (redir) return { status: 302, redirect: encodeURI(redir) };
     else return { status: 404 };
   };
 </script>
