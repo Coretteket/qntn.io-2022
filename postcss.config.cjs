565d9bd8ca317323d07dba915cc62ec2f1d4d63f
Quinten Coret <Qcoret@gmail.com>
2022-06-20 21:41:20 +0200

    Adding postcss-short-var, Fraunces font, and env variables.

--- postcss.config.cjs
+++ postcss.config.cjs
@@ -1,8 +1,18 @@
+const postcssShortVar = require('postcss-short-var');
 const postcssPresetEnv = require('postcss-preset-env');
 const cssnano = require('cssnano');
 
 const config = {
-  plugins: [postcssPresetEnv, cssnano],
+  plugins: [
+    postcssShortVar,
+    postcssPresetEnv({
+      stage: 0,
+      importFrom: './src/app.css',
+      // TODO: stay alert to future importFrom deprecation
+      features: { 'custom-properties': { disableDeprecationNotice: true } },
+    }),
+    cssnano,
+  ],
 };
 
 module.exports = config;
