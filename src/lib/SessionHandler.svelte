c8343a0157e728c5e29a187b45796efc0587376f
Quinten Coret <Qcoret@gmail.com>
2022-06-20 21:43:28 +0200

    Minor fixes.

--- src/lib/SessionHandler.svelte
+++ src/lib/SessionHandler.svelte
@@ -1,22 +1,21 @@
 <script>
   import { theme, locale } from '../scripts/stores';
-  import { onMount } from 'svelte';
-  import { browser } from '$app/env';
   import { session } from '$app/stores';
-
-  $: if (browser) {
-    document.documentElement.setAttribute('lang', $locale);
-    document.cookie = `locale=${$locale}`;
-    $session.locale = $locale;
-  }
-
-  $: if (browser) {
-    document.documentElement.setAttribute('data-theme', $theme);
-    document.cookie = `theme=${$theme}`;
-    $session.theme = $theme;
-  }
+  import { onMount } from 'svelte';
 
   onMount(() => {
+    locale.subscribe((locale) => {
+      document.documentElement.setAttribute('lang', locale);
+      document.cookie = `locale=${locale}`;
+      $session.locale = locale;
+    });
+
+    theme.subscribe((theme) => {
+      document.documentElement.setAttribute('data-theme', theme);
+      document.cookie = `theme=${theme}`;
+      $session.theme = theme;
+    });
+
     const prefers = window.matchMedia('(prefers-color-scheme: dark)').matches;
     if ($theme == 'auto') $theme = prefers ? 'dark' : 'light';
   });
--- src/lib/Translate.svelte
+++ src/lib/Translate.svelte
@@ -3,4 +3,4 @@
   export let key: string;
 </script>
 
-{$t(key)}
+{@html $t(key)}
