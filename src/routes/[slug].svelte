93c5168c667747335b3c89ea4d13b5abd8e0aed1
Quinten Coret <Qcoret@gmail.com>
2022-07-02 18:47:14 +0200

    Permanent redirect

--- src/routes/[slug].svelte
+++ src/routes/[slug].svelte
@@ -3,7 +3,7 @@
 
   export const load: Load = async ({ params: { slug } }) => {
     const redir = import.meta.env[`VITE_SLUG_${slug.toUpperCase()}`];
-    if (redir) return { status: 302, redirect: encodeURI(redir) };
+    if (redir) return { status: 301, redirect: encodeURI(redir) };
     else return { status: 404 };
   };
 </script>
