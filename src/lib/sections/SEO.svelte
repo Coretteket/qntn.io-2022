183712fdcc9a3743e4536d55bfdad8014a0417b8
Quinten Coret <Qcoret@gmail.com>
2022-08-19 17:12:32 +0800

    Attempted OG image fix

--- src/lib/sections/SEO.svelte
+++ src/lib/sections/SEO.svelte
@@ -1,8 +1,10 @@
 <script>
   import json from '../../i18n/en/index.json';
+  import { page } from '$app/stores';
 
   const title = `${json.title} ${json.subtitle}`;
   const description = json.intro;
+  const url = $page.url.host;
 </script>
 
 <svelte:head>
@@ -13,19 +15,19 @@
   <meta name="abstract" content={description} />
   <meta name="summary" content={description} />
   <meta name="author" content="Quinten Coret" />
-  <meta name="url" content="https://qntn.xyz" />
+  <meta name="url" content={url} />
 
   <!-- Open Graph / Facebook -->
   <meta property="og:type" content="website" />
-  <meta property="og:url" content="https://qntn.xyz/" />
+  <meta property="og:url" content={url} />
   <meta property="og:title" content={title} />
   <meta property="og:description" content={description} />
-  <meta property="og:image" content="/og.png" />
+  <meta property="og:image" content="{url}/og.png" />
 
   <!-- Twitter -->
-  <meta property="twitter:card" content="summary" />
-  <meta property="twitter:url" content="https://qntn.xyz/" />
+  <meta property="twitter:card" content="summary_large_image" />
+  <meta property="twitter:url" content={url} />
   <meta property="twitter:title" content={title} />
   <meta property="twitter:description" content={description} />
-  <meta property="twitter:image" content="/og.png" />
+  <meta property="twitter:image" content="{url}/og.png" />
 </svelte:head>
