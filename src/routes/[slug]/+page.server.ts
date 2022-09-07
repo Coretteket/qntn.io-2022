4d441c5c2c88e7aaf0f0e7c5dd3de2465ae907f2
Quinten Coret <Qcoret@gmail.com>
2022-09-07 23:56:44 +0200

    `export const csr = false;`

--- src/routes/[slug]/+page.server.ts
+++ src/routes/[slug]/+page.server.ts
@@ -1,8 +1,9 @@
 import { redirect, error } from '@sveltejs/kit';
-import type { PageServerLoad } from './$types';
 import { env } from '$env/dynamic/public';
+import type { PageServerLoad } from './$types';
 
 export const prerender = true;
+export const csr = false;
 
 export const load: PageServerLoad = async ({ params: { slug } }) => {
   if (!slug) throw error(404);
