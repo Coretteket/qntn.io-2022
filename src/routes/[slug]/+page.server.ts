3cbf78a850a03622d23b5ede42fb178f20008a05
Quinten Coret <Qcoret@gmail.com>
2022-09-07 23:58:06 +0200

    removing params

--- src/routes/[slug]/+page.server.ts
+++ src/routes/[slug]/+page.server.ts
@@ -2,9 +2,6 @@ import { redirect, error } from '@sveltejs/kit';
 import { env } from '$env/dynamic/public';
 import type { PageServerLoad } from './$types';
 
-export const prerender = true;
-export const csr = false;
-
 export const load: PageServerLoad = async ({ params: { slug } }) => {
   if (!slug) throw error(404);
   const url = env[`PUBLIC_SLUG_${slug.toUpperCase()}`];
