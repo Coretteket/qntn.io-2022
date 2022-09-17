a011e1a7a15d632d4bddd5dcc11ab7f87380c16c
Quinten Coret <Qcoret@gmail.com>
2022-09-17 16:34:10 +0200

    prefetch only in prod

--- src/scripts/prefetch.ts
+++ src/scripts/prefetch.ts
@@ -2,4 +2,5 @@ import { listen } from 'quicklink';
 import { slugs } from '../pages/[slug]';
 
 const ignores = (s: string) => new URL(s).pathname.replace('/', '') in slugs;
-window.addEventListener('load', listen({ ignores }));
+
+export const quicklink = () => window.addEventListener('load', listen({ ignores }));
