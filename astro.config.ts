150c1138b141c6dec2dc463ac24f5c506e1565c5
Quinten Coret <Qcoret@gmail.com>
2022-09-30 23:54:28 +0200

    astro 1.4, custom image component (shiny! hacky!), geolocation locale detection

--- astro.config.ts
+++ astro.config.ts
@@ -1,11 +1,14 @@
 import { defineConfig } from 'astro/config';
-import prefetch from "@astrojs/prefetch";
-import image from "@astrojs/image";
+import prefetch from '@astrojs/prefetch';
+import image from '@astrojs/image';
+import purgecss from 'astro-purgecss';
 
 // https://astro.build/config
 export default defineConfig({
   site: 'https://qntn.xyz',
-  integrations: [prefetch(), image({
-    serviceEntryPoint: '@astrojs/image/sharp'
-  })]
+  integrations: [
+    prefetch(),
+    image({ serviceEntryPoint: '@astrojs/image/sharp' }),
+    purgecss(),
+  ],
 });
--- middleware.ts
+++ middleware.ts
@@ -1,27 +1,29 @@
 import { parse } from "cookie";
 import { locales } from "./src/scripts/types";
 import { isLocale } from "./src/scripts/utils";
-import { next, rewrite } from '@vercel/edge';
+import { geolocation, next, rewrite } from '@vercel/edge';
 
 /** Returns true if path is localized route or a public file. */
 export const matches = ({ pathname }: URL) =>
   (new RegExp(`(^\\/(${locales.join('|')})(\\W|$)|\\.\\w+$)`)).test(pathname);
 
-/** Parses language from request headers, based on `accept-language` or cookies. */
-const parseHeaders = ({ headers }: Request) => {
-  const cookies = parse(headers.get('cookie') ?? '');
+/** Parses language from request headers, based on stored cookies, the presence
+ * of Dutch in the `accept-language` header, and geolocation. */
+const parseHeaders = (request: Request) => {
+  const cookies = parse(request.headers.get('cookie') ?? '');
   if (isLocale(cookies.locale)) return cookies.locale;
-  const acceptLanguage = headers.get('accept-language') ?? '';
-  return acceptLanguage.includes('nl') ? 'nl' : 'en';
+  const acceptLanguage = request.headers.get('accept-language') ?? '';
+  const country = geolocation(request).country;
+  return (acceptLanguage.includes('nl') || country === 'NL') ? 'nl' : 'en';
 };
 
-const headers = { headers: { 'cache-control': 'public, max-age=3600' } };
+const headers = { 'cache-control': 'public, max-age=86400' };
 
 /** Intercepts requests and rewrites them to the correct locale and applies cache headers. */
 export default function middleware(request: Request) {
   const url = new URL(request.url);
-  if (matches(url)) return next(headers);
+  if (matches(url)) return next({ headers });
   const locale = parseHeaders(request);
   url.pathname = locale + url.pathname;
-  return rewrite(url, headers);
+  return rewrite(url, { headers });
 }
